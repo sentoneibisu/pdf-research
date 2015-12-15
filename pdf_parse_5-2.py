04c6d3a0e4513d5baf1a1e84b1ebb35639eb5e9e
Kengo Kon <sazaesanwa.kimo.kunai@gmail.com>
2015-12-15 11:35:56 -0800

    [fix]辞書/配列オブジェクトを抽出する正規表現の修正
    辞書/配列内に特定の文字列(e.g. 辞書の場合:(<<),配列の場合:([))が含まれているときの不具合を修正

--- pdf_parse_5-2.py
+++ pdf_parse_5-2.py
@@ -20,9 +20,14 @@ pString2 = re.compile(r'\s*(<([a-zA-Z0-9]{2}\s*)+>)\s*')
 # zenntei : mojiretu nado no naka ni '(' ya ')' ya '<' ya '>' ya '[' ya ']' ga nai koto!!
 pString = regex.compile(r'\s*(?<rec>\((?:[^()]+|(?&rec))*\))\s*')
 #pDict = regex.compile(r'\s*(?<rec><<(?:[^<>]+|(?&rec))*>>)\s*')
-pDict = regex.compile(r'\s*(?<rec><<(?:(?:[^<>]+(?:[<>]?|><|<>))+|(?&rec))*>>)\s*')
-#pArray = regex.compile(r'\s*(?<rec>\[(?:[^\[\]]+|(?&rec))*\])\s*')
-pArray = regex.compile(r'\s*(?<rec>\[(?:[^\[\]]*|(?&rec))*\])\s*')
+#pDict = regex.compile(r'\s*(?<rec><<(?:(?:[^<>]+(?:[<>]?|><|<>))+|(?&rec))*>>)\s*')
+##pDict = regex.compile(r'\s*(?<dic><<(?<in>(?:[^<>(]+(?:[<>]?|><|<>))*|(?&str)(?&in)|(?&dic))*>>)\s*'
+##                      r'(?<str>\((?:[^()]+|(?&str))*\)){0}')
+pDict = regex.compile(r'\s*(?<dic><<((?:[^<>(]+(?:[<>]?|><|<>))*|(?&str)|(?&dic))*>>)\s*'
+                      r'(?<str>\((?:[^()]+|(?&str))*\)){0}')
+#pArray = regex.compile(r'\s*(?<rec>\[(?:[^\[\]]*|(?&rec))*\])\s*')
+pArray = regex.compile(r'\s*(?<arr>\[(?:[^\[\](]*|(?&str)|(?&arr))*\])\s*'
+                       r'(?<str>\((?:[^()]+|(?&str))*\)){0}')
 
 def Main():
     target_pdf = sys.argv[1]
